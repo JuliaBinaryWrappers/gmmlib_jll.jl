f0a339bb9cffb30ea44d924a2816d126af9bdb92
jlbuild <juliabuildbot@gmail.com>
2026-05-04 15:49:00 +0000

    gmmlib_jll build 22.7.2+0

--- src/gmmlib_jll.jl
+++ src/gmmlib_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("gmmlib")
-JLLWrappers.@generate_main_file("gmmlib", UUID("09858cae-167c-5acb-9302-fddc6874d481"))
+JLLWrappers.@generate_main_file("gmmlib", Base.UUID("09858cae-167c-5acb-9302-fddc6874d481"))
 end  # module gmmlib_jll
